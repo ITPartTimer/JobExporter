148747ed21425dfa84955c5c5247683ea751b6f9
Scott Clemons <scott.clemons317@outlook.com>
2020-02-06 19:36:08 -0700

    Commit all work

---
+++ JOBExporter/CoptPaste.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOBExporter
+{
+    #region Expand Stratix Arbor
+    // Get list of start Pos for each setup
+    List<int> lstStartPos = new List<int>();
+
+            foreach (Consume c in lstConsume)
+            {
+                int p = 1;
+
+                // Start of 1st setup is position 1 on arbor
+                if (c.Pos != 1)
+                    p = c.Pos;
+
+                lstStartPos.Add(p);
+            }
+
+// Expand Arbor from Stratix arbor
+List<ArborStratix> lstExp = new List<ArborStratix>();
+
+int aPos = 0;
+int aSetupCount = 0;
+
+// First setup Pos=1, but 1st object in List<> = 0
+int aSetup = lstStartPos[aSetupCount];
+
+            foreach (ArborStratix a in lstArborStratix)
+            {
+                //Exp Nbr into Nbr object in lstExp
+                for (int i = 1; i <= a.Nbr; i++)
+                {
+                    // Start Pos counter with 1, then ++ for each pass
+                    aPos++;
+
+                    // Look ahead to start Pos of next setup
+                    if (aSetupCount <= lstStartPos.Count)
+                    {
+                        // If current Pos = start of next setup, move to next setup
+                        if (lstStartPos[aSetupCount + 1] == aPos)
+                            aSetup++;
+                    }
+
+                    ArborStratix aExp = new ArborStratix();
+
+aExp.Job = string.Concat(job.ToString(), "-", aSetup.ToString());
+aExp.Wdth = a.Wdth;
+                    //aExp.Pos = aPos;
+                    aExp.Nbr = 1; //Expanded each cut has a qty = 1
+
+                    lstExp.Add(aExp);
+                }
+            }
+
+            Console.WriteLine("==== ARBOR EXAPNDED ====");
+            foreach (ArborStratix f in lstExp)
+            {
+                Console.WriteLine(f.Job + " / " + f.Wdth.ToString() + " / " + f.Nbr.ToString());
+            }
+
+            var lstArborCol = lstExp.GroupBy(x => x.Job);
+
+List<ArborStratix> lstArborSetups = new List<ArborStratix>();
+
+Console.WriteLine("==== ARBOR COLLAPSED INTO SETUPS ====");
+            foreach (var sGroup in lstArborCol)
+            {
+                foreach (ArborStratix a in sGroup)
+                {
+                    ArborStratix arb = new ArborStratix();
+
+arb.Job = a.Job;
+                    arb.Wdth = a.Wdth;
+                    arb.Nbr = a.Nbr;
+
+                    lstArborSetups.Add(arb);
+                }
+            }
+
+            Console.WriteLine(" ==== SETUPS: " + lstArborSetups.Count().ToString() + " ====");
+            foreach (ArborStratix a in lstArborSetups)
+            {
+                Console.WriteLine(a.Job + " / " + a.Wdth.ToString() + " / " + a.Nbr.ToString());
+            }
+
+            #endregion
+
+            /*
+            Build List<MultFile>
+            */
+
+            List<MultFile> lstMultFile = new List<MultFile>();
+
+            if (lstConsume.Count == 1)
+            {
+                // Only one setup
+                foreach (ArborStratix a in lstArborStratix)
+                {
+                    MultFile m = new MultFile();
+
+m.Job = lstNumSetups[0];
+                    m.Qty = a.Nbr;
+                    m.Size = a.Wdth;
+
+                    // Find matchind Wdth in lstDetail. 
+                    // There is NO link between Wdth in ArborStratix and Wdth on SO
+                    var detail = lstDetail.Where(x => x.Wdth == a.Wdth).Distinct();
+
+                    foreach(MultDetail d in detail)
+                    {
+                        // Should only be one object
+                        m.WdthP = d.WdthP;
+                        m.WdthN = d.WdthN;
+                    }                  
+
+                    lstMultFile.Add(m);
+                }
+            }
+            else
+            {
+                // More than one setup
+
+                //// Get list of start Pos for each setup
+                //List<int> lstStartPos = new List<int>();
+
+                //foreach (Consume c in lstConsume)
+                //{
+                //    int p = 1;
+
+                //    // Start of 1st setup is position 1 on arbor
+                //    if (c.Pos != 1)
+                //        p = c.Pos;
+
+                //    lstStartPos.Add(p);
+                //}
+
+                //// Expand Arbor from Stratix arbor
+                //List<ArborExp> lstExp = new List<ArborExp>();
+
+                //int aPos = 0;
+                //int aSetupCount = 0;
+
+                //// First setup Pos=1, but 1st object in List<> = 0
+                //int aSetup = lstStartPos[aSetupCount];
+
+                //foreach(ArborStratix a in lstArborStratix)
+                //{   
+                //    //Exp Nbr into Nbr object in lstExp
+                //    for (int i = 1; i <= a.Nbr; i++)
+                //    {
+                //        // Start Pos counter with 1, then ++ for each pass
+                //        aPos++;
+
+                //        // Look ahead to start Pos of next setup
+                //        if (aSetupCount <= lstStartPos.Count)
+                //        {
+                //            // If current Pos = start of next setup, move to next setup
+                //            if (lstStartPos[aSetupCount + 1] == aPos)
+                //                aSetup++;
+                //        }
+
+                //        ArborExp aExp = new ArborExp();
+
+                //        aExp.Job = string.Concat(job.ToString(),"-",aSetup.ToString());
+                //        aExp.SetUp = aSetup;
+                //        aExp.Wdth = a.Wdth;
+                //        aExp.Pos = aPos;
+
+                //        lstExp.Add(aExp);
+                //    }
+                //}
+
+                //// test
+                //Console.WriteLine("==== ARBOR EXP ====");
+                //foreach (ArborExp f in lstExp)
+                //{
+                //    Console.WriteLine(f.Job + " / " + f.SetUp.ToString() + " / " + f.Wdth.ToString() + " / " + f.Pos.ToString());
+                //}
+
+                //// Break ArborFull into separate setups by grouping on ArborFull.Setup
+                //var lstSetupsAll = lstExp.GroupBy(x => x.SetUp);
+
+                //// NOTE: NEED TO GROUP BY WDTH AND PUT IN NEW LIST
+
+                ///*
+                //Build MultFile
+
+                //lstSetupsAll is an iGrouping<>
+                //Need to nest  the object foreach() inside a var foreach()
+                //*/
+                //foreach(var sGroup in lstSetupsAll)
+                //{
+                //    foreach (ArborExp a in sGroup)
+                //    {
+                //        MultFile m = new MultFile();
+
+                //        m.Job = a.Job;
+                //        m.Qty = 1; // NEED TO FIX
+                //        m.Size = a.Wdth;
+
+                //        var detail = lstDetail.Where(x => x.Wdth == a.Wdth).Distinct();
+
+                //        foreach (MultDetail d in detail)
+                //        {
+                //            // Should only be one object
+                //            m.WdthP = d.WdthP;
+                //            m.WdthN = d.WdthN;
+                //        }
+
+                //        lstMultFile.Add(m);
+                //    }
+                    
+                //}
+            }
+
+            //testing
+            Console.WriteLine("==== MULTFILE ====");
+            foreach (MultFile m in lstMultFile)
+            {
+                Console.WriteLine(m.Job + " / " + m.Cust + " / " + m.Qty.ToString() + " / " + m.Size.ToString() + " / " + m.WdthP.ToString() + " / " + m.WdthN.ToString() + " / " + m.Knife.ToString());
+            }
+}
+
+    // Add last Pos on arbor.  +1, since stopPos = -1
+            lstStartPos.Add(lstArborStratix.Sum(x => x.Nbr) + 1);
+
+            List<ArborStratix> lstMults = new List<ArborStratix>();
+
+int accPos = 0;
+int setupCnt = 1;
+int partialNbr = 0;
+int balNbr = 0;
+int posStratixArbor = 0;
+
+            foreach (int p in lstStartPos)
+            {   
+               
+                int stopPos = p - 1;
+
+                // Step thru each member of lstArborStratix
+                for (int i = posStratixArbor; i<lstArborStratix.Count(); i++)
+                {
+                    int accNbr = lstArborStratix[i].Nbr + accPos;
+
+                    if (accNbr <= stopPos)
+                    {
+                        // Update the Job to represent the setup -1,-2, and so on
+                        lstArborStratix[i].Job = string.Concat(job.ToString(), "-", setupCnt.ToString());
+accPos = accPos + lstArborStratix[i].Nbr;
+                    }
+                    else
+                    {
+                        lstArborStratix[i].Job = string.Concat(job.ToString(), "-", setupCnt.ToString());
+
+// Figure how many Nbr your need from lstMult[i].Nbr and the balance
+partialNbr = lstArborStratix[i].Nbr - (stopPos - accPos);
+                        balNbr = lstArborStratix[i].Nbr - partialNbr;
+
+                        // If there is a partial, update current member and add member with balance
+                        if (partialNbr != lstArborStratix[i].Nbr)
+                        {
+                            // Update Nbr of just added member to what you need to complete this setup
+                            lstArborStratix[i].Nbr = partialNbr;
+
+                            // Move to new setup
+                            setupCnt++;
+
+                            // Create a new member for the List that contains the balance of Nbr
+                            // This is on a new setup
+                            ArborStratix a = new ArborStratix();
+
+a.Job = string.Concat(job.ToString(), "-", setupCnt.ToString());
+a.Wdth = lstArborStratix[i].Wdth;
+                            a.Nbr = balNbr;
+
+                            // All new member to List right after current index
+                            lstArborStratix.Insert((i + 1), a);
+                        }
+                        
+                        posStratixArbor = 1 + 1; // Might need to be +2
+                        i = lstArborStratix.Count();
+                    }
+                }              
+            }
+
+            Console.WriteLine("==== UPDATED ARBOR ====");
+            foreach (ArborStratix f in lstArborStratix)
+            {
+                Console.WriteLine(f.Job + " / " + f.Wdth.ToString() + " / " + f.Nbr.ToString());
+            }
--- JOBExporter/Program.cs
+++ JOBExporter/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JOBExporter.Models;
+using JOBExporter.DAL;
+using JOBExporter.XLS;
 
 namespace JOBExporter
 {
@@ -10,6 +13,336 @@ namespace JOBExporter
     {
         static void Main(string[] args)
         {
+            #region Args
+            List<string> sArgs = new List<string>();
+
+            string job = string.Empty;
+
+            Console.WriteLine("Enter Job:");
+            job = Console.ReadLine();
+
+            //try
+            //{
+            //    if (args.Length > 0)
+            //        job = args[0].ToString();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Arg ex: " + ex.Message);
+            //}
+            #endregion
+
+            #region Consume
+            /*
+            Get Planned Consumption from Stratix.
+            The number of rows is the number of setups on this Job
+            Sort by Seq.  Use Pos information to determine where 
+            at what arbor position to start each setup
+            */
+            DataAccess objConsume = new DataAccess();
+
+            List<Consume> lstConsume = new List<Consume>();
+
+            lstConsume = objConsume.Get_Consumed(job);
+
+            // Testing Change Pos on 2nd element to 12
+            //var editConsume = from c in lstConsume
+            //              where c.Pos == 16
+            //              select c;
+
+            //foreach(Consume c in editConsume)
+            //{
+            //    c.Pos = 18;
+            //}
+
+            // test
+            Console.WriteLine("==== CONSUME ====");
+            foreach (Consume c in lstConsume)
+            {
+                Console.WriteLine(c.Job.ToString() + " / " + c.Seq.ToString() + " / " + c.Tag + " / " + c.Wdth.ToString() + " / " + c.Stp.ToString() + " / " + c.Pos.ToString());
+            }
+            #endregion           
+
+            #region SO
+            /*
+            Get Unique SOs from Stratix.
+            Planned Production iptjpp_rec will list each SO on the Job
+            Parse this into a Prefix, Ref, Item and SubItem.
+            This will be used to query Transaction Common Items joined with
+            CPS Tolerances to get item details.           
+            */
+            DataAccess objSO = new DataAccess();
+
+            List<SO> lstSO = new List<SO>();
+
+            lstSO = objSO.Get_SOs(job);
+
+            // test
+            Console.WriteLine("==== SO ====");
+            foreach (SO s in lstSO)
+            {
+                Console.WriteLine(s.Trgt + " / " + s.Pfx + " / " + s.Ref + " / " + s.Itm + " / " + s.SItm);
+            }
+
+            // Build a list of Ref + Item to be used in the query for Item Details
+            string inQry = "";
+
+            foreach (SO s in lstSO)
+            {
+                inQry = inQry + string.Concat(s.Ref, s.Itm, ",");
+            }
+
+            inQry = inQry.TrimEnd(',');
+
+            // test
+            //Console.WriteLine("inQry: " + inQry);
+            #endregion
+
+            #region MultDetail
+            /*
+            Transaction Common Item Product joined with CPS Tolerances will
+            give you all the detail you need about the cut for each SO (ex:56572-54)
+            on the Job
+            */
+            DataAccess objDetail = new DataAccess();
+
+            List<MultDetail> lstDetail = new List<MultDetail>();
+
+            lstDetail = objDetail.Get_Details(inQry);
+
+            // test
+            Console.WriteLine("==== MULT DETAIL ====");
+            foreach (MultDetail m in lstDetail)
+            {
+                Console.WriteLine(m.Pfx + " / " + m.Ref + " / " + m.Itm + " / " + m.Cus + " / " + m.Part + " / " + m.CtlNo.ToString() + " / " + m.Frm + " / " + m.Ga.ToString() + " / " + m.GaP.ToString() + " / " + m.GaN.ToString() + " / " + m.Wdth.ToString() + " / " + m.WdthP.ToString() + " / " + m.WdthN.ToString());
+            }
+            #endregion
+
+            #region Ga
+            /*
+            Get the Gauge listed on the Job
+            
+            Determine the most constrained gauge range from the CPS on the Job
+            */
+
+            // Consume query was ordered by seq, so first member contains the TagNo for the job
+            string tag = lstConsume.Select(x => x.Tag).First().ToString();
+
+            DataAccess objGa = new DataAccess();
+
+            Ga g = new Ga();
+
+            g = objGa.Get_Ga(tag, lstDetail);
+            #endregion
+
+            #region Build HdrFile          
+            /*
+            Build List<HdrFile>
+            */
+
+            // Determine number of setups
+            int numSetups = lstConsume.Count;
+            List<string> lstNumSetups = new List<string>();
+
+            //Add sufix of 1,2,3... to end of Job
+            if (numSetups == 1)
+                lstNumSetups.Add(string.Concat(job.ToString(), "-", "1"));
+            else
+                for (int i = 0; i < numSetups; i++)
+                    lstNumSetups.Add(string.Concat(job.ToString(), "-", (i + 1).ToString()));
+
+
+            List<HdrFile> lstHdr = new List<HdrFile>();
+
+            foreach (string j in lstNumSetups)
+            {
+                HdrFile h = new HdrFile();
+
+                h.Job = j;
+                h.Mtl = lstDetail[0].Frm;
+                h.Wdth = lstConsume[0].Wdth;
+                h.Ga = g.NumSize;
+                h.Clr = h.KnifeClr * h.Ga;             
+                h.GaP = g.GaP;
+                h.GaN = g.GaN;              
+
+                // After Pos = 1, if consecutive Pos are same value, you are reslitting
+                //FIX THIS TO ADD RESLIT TO NOTE
+                //if (lstConsume[j].Pos != 1)
+                //{
+                //    if (lstConsume[j].Pos == lstConsume[j - 1].Pos)
+                //        h.Note = "RESLIT";
+                //}
+
+                lstHdr.Add(h);
+            }
+
+            // testing
+            Console.WriteLine("==== HDR FILE ====");
+            foreach (HdrFile h in lstHdr)
+            {
+                Console.WriteLine(h.Job + " / " + h.Cust + " / " + h.Mtl + " / " + h.Wdth.ToString() + " / " + h.Ga.ToString() + " / " + h.KnifeClr.ToString() + " / " + h.Clr.ToString() + " / " + h.GaP.ToString() + " / " + h.GaN.ToString() + " / " + h.Note);
+            }
+            #endregion
+
+            #region Build MultFile
+            /*
+            Build MultFile
+            */
+
+            // Get Arbor from Stratix
+            DataAccess objArbor = new DataAccess();
+
+            List<ArborStratix> lstArborStratix = new List<ArborStratix>();
+
+            lstArborStratix = objArbor.Get_Arbors(job);
+
+            // test
+            Console.WriteLine("==== ARBOR ====");
+            foreach (ArborStratix a in lstArborStratix)
+            {
+                Console.WriteLine(a.Job.ToString() + " / " + a.Wdth.ToString() + " / " + a.Nbr.ToString());
+            }
+
+            // Get list of start Pos for each setup
+            List<int> lstStartPos = new List<int>();
+
+            foreach (Consume c in lstConsume)
+                lstStartPos.Add(c.Pos);
+
+            // Expand lstArborStratix to assign Job, setup and position
+            List<ArborExp> lstExp = new List<ArborExp>();
+
+            int aPos = 0; //Arbor position 1 to X
+            int aSetupCount = 0;
+
+            // Suffix on Job is current setup on Job
+            // First setup Pos=1, but 1st object in List<> = 0
+            int aSetup = lstStartPos[aSetupCount];
+
+            foreach (ArborStratix a in lstArborStratix)
+            {
+                //Look at each cut in the setup and expand into setup with only single cuts
+                for (int i = 1; i <= a.Nbr; i++)
+                {
+                    // Start position counter with 1, then ++ for each pass
+                    aPos++;
+
+                    // If there is only on setup start Pos
+                    if (lstStartPos.Count == 1)
+                        aSetup = 1;                  
+                    else
+                    {
+                        // Look ahead to start Pos of next setup
+                        if (aSetupCount <= lstStartPos.Count)
+                        {
+                            // If current position = start Pos of next setup, increment the setup count
+                            if (lstStartPos[aSetupCount + 1] == aPos)
+                                aSetup++;
+                        }       
+                    }
+
+                    ArborExp aExp = new ArborExp();
+
+                    aExp.Job = string.Concat(job.ToString(), "-", aSetup.ToString());
+                    aExp.Wdth = a.Wdth;
+                    aExp.Pos = aPos;
+
+                    lstExp.Add(aExp);
+                }
+            }
+
+            Console.WriteLine("==== ARBOR EXPANDED ====");
+            foreach (ArborExp f in lstExp)
+            {
+                Console.WriteLine(f.Job + " / " + f.Wdth.ToString() + " / " + f.Pos.ToString());
+            }
+
+
+            int indexClp = 0;
+
+            // Collapse Arbor counting number of repeat sizes as you go
+            List<ArborStratix> lstArborClp = new List<ArborStratix>();
+
+            for (int i = 0; i < lstExp.Count(); i++)
+            {
+                // 1st Position always gets added to the List
+                if (lstExp[i].Pos == 1)
+                {
+                    ArborStratix a = new ArborStratix();
+
+                    a.Job = lstExp[i].Job;
+                    a.Wdth = lstExp[i].Wdth;
+                    a.Nbr = 1; // Default to get the count started
+
+                    lstArborClp.Add(a);
+                }
+                else
+                {
+                    // If Wdth of next element is same as previous
+                    if (lstExp[i].Wdth == lstArborClp[indexClp].Wdth)
+                    {
+                        // lstExp.Wdth is same as previous lstArborClp, so just ++ Nbr
+                        lstArborClp[indexClp].Nbr++;
+                    }
+                    else
+                    {
+                        // New wdth, so add to lstArborClp
+                        ArborStratix a = new ArborStratix();
+
+                        a.Job = lstExp[i].Job;
+                        a.Wdth = lstExp[i].Wdth;
+                        a.Nbr = 1; // Default to get the count started
+
+                        lstArborClp.Add(a);
+
+                        // Inc index of lstArborClp
+                        indexClp++;
+                    }
+                }
+            }
+
+            Console.WriteLine("==== ARBOR COLLAPSED ====");
+            foreach (ArborStratix f in lstArborClp)
+            {
+                Console.WriteLine(f.Job + " / " + f.Wdth.ToString() + " / " + f.Nbr.ToString());
+            }
+
+            // Build MultFile
+            List<MultFile> lstMults = new List<MultFile>();
+
+            foreach(ArborStratix a in lstArborClp)
+            {
+                MultFile m = new MultFile();
+
+                m.Job = a.Job;
+                m.Qty = a.Nbr;
+                m.Size = a.Wdth;
+                m.WdthP = Convert.ToDecimal(lstDetail.Where(x => x.Wdth == a.Wdth).Select(x => x.WdthP).FirstOrDefault());
+                m.WdthN = Convert.ToDecimal(lstDetail.Where(x => x.Wdth == a.Wdth).Select(x => x.WdthN).FirstOrDefault());
+
+                lstMults.Add(m);
+            }
+
+            // testing
+            Console.WriteLine("==== MULT FILE ====");
+            foreach (MultFile h in lstMults)
+            {
+                Console.WriteLine(h.Job + " / " + h.Cust + " / " + h.Qty.ToString() + " / " + h.Size.ToString() + " / " + h.WdthP.ToString() + " / " + h.WdthN.ToString() + " / " + h.Knife);
+            }
+            #endregion
+
+            #region Exports
+            ExcelExport objXLS = new ExcelExport();
+
+            objXLS.WriteHdr(lstHdr);
+
+            objXLS.WriteMults(lstMults);
+            #endregion
+
+            // testing
+            Console.WriteLine("Press key to exit");
+            Console.ReadKey();
         }
     }
 }
